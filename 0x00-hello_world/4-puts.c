57ab53c0af842fcf939d31deb2a76f8e03d97023
okontapaul <pokonta@yahoo.com>
2022-12-08 14:57:16 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,6 @@
-#!/bin bash 
+#!/bin/bash
 #include <stdio.h>
-/**
- * Description: main - Puts the texts to stdout.
- * Return: 0 should code runs successfully.
- */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
