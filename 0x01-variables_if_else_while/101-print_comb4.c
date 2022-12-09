fb23f31667c17be9709a213c2251c242318d7d2a
okontapaul <pokonta@yahoo.com>
2022-12-09 15:31:39 -0800

    success

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 
 /**
- * Description: main - program that prints all possible different combs
- * Numbers must be separated by ,followed by a space
- * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
+ * Description: main - A program that prints all possible different comb
  * Return: 0
  */
 
 int main(void)
 {
-int num1 = 48;
-int num2;
-int com = 44;
-while (num1 <= 57)
-{
-num2 = num1 + 1;
-while (num2 <= 57)
-{
-putchar(num1);
-putchar(num2);
-if (num1 != 56 || num2 != 57)
-{
-putchar(com);
-putchar(32);
-}
-num2 += 1;
-}
-num1 += 1;
-}
-putchar('\n');
-return (0);
+	int num1 = 48;
+	int num2, num3;
+	int comb = 44;
+
+	while (num1 <= 57)
+	{
+		num2 = num1 + 1;
+		while (num2 <= 57)
+		{
+			num3 = num2 + 1;
+			while (num3 <= 57)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 != 55 || num2 != 56 || num3 != 57)
+				{
+					putchar(comb);
+					putchar(32);
+				}
+				num3 += 1;
+			}
+			num2 += 1;
+		}
+		num1 += 1;
+	}
+	putchar('\n');
+	return (0);
 }
