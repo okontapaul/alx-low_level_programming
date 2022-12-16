205bc15989250b7c114a23ace45fbba340814e1f
okontapaul <pokonta@yahoo.com>
2022-12-15 17:30:59 -0800

    Yinka

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,37 +1,26 @@
-#include "main.h"
+#include"main.h"
 
 /**
- * print_number - a function that prints an integer
- * @n: insert num
- * Return: 0
- */
+ * print_number - Prints an integer.
+ *
+ * @n: The integer to be printed (output).
+*/
 
 void print_number(int n)
 {
-unsigned int n1, n2;
-int i;
-int aux = 1;
+	unsigned int num = n;
 
-if (n < 0)
-{
-n = n * -1;
-_putchar('-');
-}
-n1 = n;
-n2 = n1;
-if (n1 > 9)
-{
-while (n1 >= 10)
-{
-aux = aux * 10;
-n1 = n1 / 10;
-}
-_putchar((n2 / aux) + '0');
-aux = aux / 10;
+	/check if it's negative/
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -num;
+	}
 
-for (i = aux; i >= 1; i = i / 10)
-_putchar((n2 / i) % 10 + '0');
-}
-else
-_putchar(n1 + '0');
+	/then print the first few digits/
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	/Then print the last digit/
+	_putchar((num % 10) + 48);
 }
