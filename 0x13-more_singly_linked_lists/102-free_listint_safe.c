218abf25b7412be020f613995c16035fbd286876
okontapaul <pokonta@yahoo.com>
2023-01-31 20:51:35 -0800

    Second

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,40 +1,63 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - frees a linked list
- * @h: pointer to the first node in the linked list
- *
- * Return: number of elements in the freed list
+ * _ra - a function that reallocates memory for an array of pointers
+ * to the nodes in a linked list.
+ * @list: old list to append.
+ * @size: size of the new list (always one more than the old list).
+ * @new: new node to add to the list.
+ * Return: new list pointer.
  */
-size_t free_listint_safe(listint_t **h)
+listint_t **_ra(listint_t **list, size_t size, listint_t *new)
 {
-	size_t len = 0;
-	int diff;
-	listint_t *temp;
+	listint_t **newlist;
+	size_t x;
 
-	if (!h || !*h)
-		return (0);
-
-	while (*h)
+	newlist = malloc(size * sizeof(listint_t *));
+	if (newlist == NULL)
 	{
-		diff = *h - (*h)->next;
-		if (diff > 0)
-		{
-			temp = (*h)->next;
-			free(*h);
-			*h = temp;
-			len++;
-		}
-		else
-		{
-			free(*h);
-			*h = NULL;
-			len++;
-			break;
-		}
+		free(list);
+		exit(98);
 	}
+	for (x = 0; x < size - 1; x++)
+		newlist[x] = list[x];
+	newlist[x] = new;
+	free(list);
+	return (newlist);
+}
 
-	*h = NULL;
+/**
+ * free_listint_safe - a function that frees a listint_t list.
+ * @head: double pointer to the start of the list.
+ * Return: size ot the list that was free'd.
+ */
+size_t free_listint_safe(listint_t **head)
+{
+	size_t x, num = 0;
+	listint_t **list = NULL;
+	listint_t *next;
 
-	return (len);
+	if (head == NULL || *head == NULL)
+	{
+		return (num);
+	}
+	while (*head != NULL)
+	{
+		for (x = 0; x < num; x++)
+		{
+			if (*head == list[x])
+			{
+				*head = NULL;
+				free(list);
+				return (num);
+			}
+		}
+		num++;
+		list = _ra(list, num, *head);
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+	}
+	free(list);
+	return (num);
 }
